65d23ab2cd7a1a5fa03bbd9166c332df87618214
JordanDickersonRev <jordanmatthewdickerson@gmail.com>
2026-03-03 19:55:23 -0500

    Updated resume button

--- PortfolioSite.Client/Components/Sections/IntroductionSection.razor.cs
+++ PortfolioSite.Client/Components/Sections/IntroductionSection.razor.cs
@@ -4,7 +4,6 @@ namespace PortfolioSite.Client.Components.Sections
 {
     public partial class IntroductionSection
     {
-        private readonly string _resumeDownloadUrl = "/files/resume.pdf";
-        private readonly string _resumeFileName = "resume.pdf";
+        private readonly string _resumeUrl = "https://docs.google.com/document/d/194nEzxZTs5WYbBkVFMpmXn0fEmhI9CFOsR73qBjWMvk/edit?usp=sharing";
     }
 }
